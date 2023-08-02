ceed59ce1bec515adb888e89f7a1549f81fc64d8
Olivia Motevalli <olivia.motevalli.aliabadi@gmail.com>
2023-08-02 10:57:01 +0100

    new app added initial styling

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>BRAND NEW APP</Text>
+      <Text style={styles.textStyle}>Rendezvous</Text>
+      <TextInput style={styles.inputStyle} placeholder="Username"></TextInput>
+      <TextInput style={styles.inputStyle} placeholder="Password"></TextInput>
+      <Button color={"#573295"} title={"Login"} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ffafcc",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textStyle: {
+    fontSize: 50,
+    color: "#fff",
+    fontFamily: "monospace",
+  },
+  inputStyle: {
+    margin: 20,
+    padding: 10,
+    width: "80%",
+    backgroundColor: "#fff",
   },
 });
