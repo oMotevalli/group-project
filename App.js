1b4e59853b5d12b3650fbd46ce9bc5020cc800d0
zahraakarim <nikishakarim@gmail.com>
2023-08-03 10:34:31 +0100

    reworking the styling

--- App.js
+++ App.js
@@ -2,13 +2,11 @@ import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import styles from "./styling";
 import WelcomeLogin from "./Components/WelcomeLogin";
-import BottomNavigation from "./Components/BottomNavigation";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <WelcomeLogin />
-      <BottomNavigation />
       <StatusBar style="auto" />
     </View>
   );
--- styling.js
+++ styling.js
@@ -8,7 +8,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   headerStyle: {
-    fontSize: 60,
+    fontSize: 80,
     color: "#fff",
     fontFamily: "PinkyStyle",
     textAlign: "center",
