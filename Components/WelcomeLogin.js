df2debe22ed9006604f58bb4ec9fb5fa5831f18e
Milosz Slezak <miloszoxo1982@gmail.com>
2023-08-04 16:35:12 +0100

    ads funcionality to log in and sign form

--- Components/WelcomeLogin.js
+++ Components/WelcomeLogin.js
@@ -2,23 +2,75 @@ import { StatusBar } from "expo-status-bar";
 import styles from "../styling";
 import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import CustomHeader from "../src/screens/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const WelcomeLogin = ({ navigation }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [users, setUsers] = useState();
+  const [isNotCorrect, setIsNotCorrect] = useState(false);
+  const [refreshDb, setRefreshDb] = useState(false);
+  useEffect(() => {
+    axios
+      .get("https://rendezvous-backend.onrender.com/api/users")
+      .then(({ data }) => {
+        setUsers(data.users);
+      });
+  }, [refreshDb]);
+  const handleLogin = () => {
+    users.find((user) => {
+      if (user.username === username && user.password === password) {
+        setIsNotCorrect(false);
+        navigation.navigate("NavTabs");
+      } else {
+        setIsNotCorrect(true);
+      }
+    });
+  };
   return (
     <View style={styles.container}>
       <CustomHeader />
       <View style={styles.inputContainer}>
-        <TextInput style={styles.inputStyle} placeholder="Username"></TextInput>
-        <TextInput style={styles.inputStyle} placeholder="Password"></TextInput>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="Username"
+          onFocus={() => {
+            setIsNotCorrect(false);
+            refreshDb ? setRefreshDb(false) : setRefreshDb(true);
+          }}
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardAppearance="default"
+          onChangeText={(newText) => setUsername(newText)}
+        ></TextInput>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="Password"
+          onFocus={() => setIsNotCorrect(false)}
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardAppearance="default"
+          secureTextEntry={true}
+          onChangeText={(newText) => setPassword(newText)}
+        ></TextInput>
+      </View>
+      <View style={{ alignItems: "center" }}>
+        {isNotCorrect && (
+          <Text style={{ fontSize: 10, color: "red" }}>
+            Username or Password is not correct!
+          </Text>
+        )}
       </View>
       <View style={styles.loginButtonContainer}>
         <TouchableOpacity
           style={styles.loginButton}
-          onPress={() => navigation.navigate("NavTabs")}
+          onPress={() => handleLogin()}
         >
           <Text style={styles.loginButtonText}>Login</Text>
         </TouchableOpacity>
       </View>
+
       <Text style={styles.noAccountText}>Don't have an account?</Text>
       <TouchableOpacity onPress={() => navigation.navigate("Create")}>
         <Text style={styles.signUpHereText}>Sign up here!</Text>
