1300252a1acb6097f29e85e9d3a2113450063eab
Olivia Motevalli <olivia.motevalli.aliabadi@gmail.com>
2023-08-07 16:46:08 +0100

    map functionality

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,47 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, StyleSheet, View } from "react-native";
 import MapView from "react-native-maps";
 import { Marker, Callout } from "react-native-maps";
+import axios from "axios";
 
 const MapScreen = () => {
   const [region, setRegion] = useState({
-    // longitude: -2.242631,
-    // latitude: 53.480759,
-    // longitudeDelta: 0.04,
-    // latitudeDelta: 0.04,
-    longitude: -122.4324,
-    latitude: 37.78825,
+    longitude: -2.242631,
+    latitude: 53.480759,
     longitudeDelta: 0.04,
-    latitudeDelta: 0.09,
+    latitudeDelta: 0.04,
+    // longitude: -122.4324,
+    // latitude: 37.78825,
+    // longitudeDelta: 0.04,
+    // latitudeDelta: 0.09,
   });
   const [pin, setPin] = useState({ longitude: -122.4324, latitude: 37.78825 });
+  const [events, setEvents] = useState([]);
 
   //   probably need function to change the region
 
+  //API CALL
+  const getEvents = () => {
+    return axios
+      .get("https://rendezvous-backend.onrender.com/api/user_ideas")
+      .then((response) => {
+        console.log(response.data.ideas);
+        return response.data.ideas;
+      });
+  };
+
+  useEffect(() => {
+    getEvents().then((data) => setEvents(data));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Rendezvous</Text>
       <MapView style={styles.map} region={region} provider="google">
-        <Marker
-          coordinate={pin}
-          pinColor={"blue"}
-          draggable={true}
-          onDragStart={(e) => {
-            setPin({
-              latitude: e.nativeEvent.coordinate.latitude,
-              longitude: e.nativeEvent.coordinate.longitude,
-            });
-          }}
-          onDragEnd={(e) => {
-            console.log("Drag end:", e.nativeEvent.coordinates);
-          }}
-        >
-          <Callout>
-            <Text>You</Text>
-          </Callout>
-        </Marker>
+        {events.map((event) => (
+          <Marker
+            key={event.id}
+            coordinate={{
+              latitude: event.latitude,
+              longitude: event.longitude,
+            }}
+            title={event.name}
+          >
+            <Callout>
+              <View style={styles.calloutContainer}>
+                <Text style={styles.calloutText}>{event.title}</Text>
+                <Text>{event.address}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
       </MapView>
-      <Text>Hello</Text>
     </View>
   );
 };
@@ -59,8 +72,41 @@ const styles = StyleSheet.create({
   map: {
     margin: 50,
     width: "80%",
-    height: "50%",
+    height: "70%",
+  },
+  // // calloutContainer: {
+  // //   width: 100,
+  // //   height: 200,
+  // // },
+  calloutText: {
+    fontWeight: "bold",
+    // padding: 100,
   },
 });
 
 export default MapScreen;
+
+{
+  /* <Marker
+coordinate={pin}
+  pinColor={"blue"}
+  draggable={true}
+  onDragStart={(e) => {
+    setPin({
+      latitude: e.nativeEvent.coordinate.latitude,
+      longitude: e.nativeEvent.coordinate.longitude,
+    });
+  }}
+  onDragEnd={(e) => {
+    console.log("Drag end:", e.nativeEvent.coordinates);
+  }}
+> */
+}
+{
+  /* <Callout>
+        <Text>You</Text>
+      </Callout>*/
+}
+{
+  /* </Marker> */
+}
